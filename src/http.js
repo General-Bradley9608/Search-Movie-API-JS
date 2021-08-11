6b1e5e0ecbff109d05a67585ad0e9fda28f13c97
General-Bradley9608 <bradleyhelphenstine9608@gmail.com>
2021-08-11 03:14:04 +0300

    add post request

--- src/http.js
+++ src/http.js
@@ -5,3 +5,15 @@ class EasyHTTP {
     const resData = await response.json();
     return resData;
   }
+
+  // Make an HTTP POST Request
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+
+    const resData = await response.json();
+    return resData;
+  }
